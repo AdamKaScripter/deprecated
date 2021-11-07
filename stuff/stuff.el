fc312334bbaac39289488e36a5f3b1b7e9b8b394
Adam Kandur <rndd@tuta.io>
2021-11-07 20:43:05 +0300

    add stuff-init

--- stuff/stuff.el
+++ stuff/stuff.el
@@ -1,7 +1,14 @@
-(setq inhibit-startup-screen t)
-(menu-bar-mode 0)
-(tool-bar-mode 0)
+(provide 'stuff)
 
-(set-frame-font "Terminus 20" nil t)
+(defun stuff-init ()
+  (interactive)
+  (stuff-emacs-options-to-apply))
 
-(setq backup-directory-alist '(("." . "~/.emacs_saves")))
+(defun stuff-emacs-options-to-apply ()
+  (setq inhibit-startup-screen t)
+  (menu-bar-mode 0)
+  (tool-bar-mode 0)
+
+  (set-frame-font "Terminus 20" nil t)
+
+  (setq backup-directory-alist '(("." . "~/.emacs_saves"))))

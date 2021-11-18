af24ae615b55092c883426a5f7477fd9d246cc7b
Adam Kandur <rndd@tuta.io>
2021-11-19 00:20:52 +0300

    update c.scm

--- c.scm
+++ c.scm
@@ -43,6 +43,7 @@
                 "0anfg24whrmjgfh24akc6i78gbm4fg8rq55nyni4qahp8adysxq3"))))
     (build-system cmake-build-system)
     (native-inputs `(("libpng" ,libpng)))
+    (propagated-inputs `(("libpng" ,libpng)))
     ;; (arguments
     ;;  '(#:configure-flags
     ;;    '("-DBUILD_SHARED_LIBS=ON")))

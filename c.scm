533c3fa33b83c9507a16a30375cb5ba26b41b0a0
Adam Kandur <rndd@tuta.io>
2021-11-19 00:25:08 +0300

    update c.scm

--- c.scm
+++ c.scm
@@ -8,6 +8,7 @@
   #:use-module (guix build-system gnu)
   #:use-module (guix build-system trivial)
   #:use-module (gnu packages)
+  #:use-module (gnu packages sdl)
   #:use-module (gnu packages image)
   #:use-module (gnu packages bash)
   #:use-module (gnu packages bootstrap)

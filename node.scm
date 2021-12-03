a35ef2401205f983347c2f92bced9fcf59fe33fa
Adam Kandur <rndd@tuta.io>
2021-12-03 12:32:54 +0300

    udpate node

--- node.scm
+++ node.scm
@@ -9,6 +9,7 @@
   #:use-module (guix build-system gnu)
   #:use-module (guix build-system node)
   #:use-module (gnu packages)
+  #:use-module (gnu packages node)
   #:use-module (gnu packages adns)
   #:use-module (gnu packages base)
   #:use-module (gnu packages bash)

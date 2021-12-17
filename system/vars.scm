71813b475352f1c90154c1f1aa863ccf472c894c
Adam Kandur <rndd@tuta.io>
2021-12-17 12:42:04 +0300

    ojdsapjodsapoja

--- system/vars.scm
+++ system/vars.scm
@@ -4,15 +4,16 @@
   #:export (%my-system-packages))
 
 (define %my-desktop-services
-  (append
-   (map specification->package
-	`("emacs"
-	  "emacs-exwm"
-	  "emacs-desktop-environment"
-	  "emacs-magit"
-	  "emacs-stuff"
-	  "emacs-emojify"
-	  "emacs-editorconfig"
-	  "papirus-icon-theme"
-	  "nss-certs"))
-   %base-packages))
+  ;; (append
+  ;;  (map specification->package
+  ;;       `("emacs"
+  ;;         "emacs-exwm"
+  ;;         "emacs-desktop-environment"
+  ;;         "emacs-magit"
+  ;;         "emacs-stuff"
+  ;;         "emacs-emojify"
+  ;;         "emacs-editorconfig"
+  ;;         "papirus-icon-theme"
+  ;;         "nss-certs"))
+  ;;  %base-packages))
+  "test")
